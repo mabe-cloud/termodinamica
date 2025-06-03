df921325029cdc89065abab845db08c1701060ae
Maria Beatriz <mbeatrizmargutti@gmail.com>
2025-06-02 21:12:06 -0300

    Update main.py
    Apenas eliminei o uso da biblioteca Barril.

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from BlackOil import GasPhase_Correlations
 from BlackOil import OilPhase_Correlations
-from barril.units import Scalar
-from barril.units import ChangeScalars
+
 
 # Dados do problema
-T = Scalar(122, 'degF')
-P = Scalar(3626, 'psia')
-Pb = Scalar(5000, 'psia')
+T = 122
+P = 3626
+
+unidades = ['psia', 'F']
+Pb = 5000
+unidades2= ['psia', 'psia', 'F']
 do = 0.86
 dg = 0.84
 
-T = Scalar(T.GetValue('degR'),'degR')
-print(T)
-
 print('### Gas ###')
-GasPhase_Correlations(dg,P,T).output(dg,P,T)
+GasPhase_Correlations(dg, P, T, unidades).output()
 print('')
-"""
+
 print('### Oil ###')
-OilPhase_Correlations(do=do, dg=dg, API=None, P=P, Pb=Pb, T=T).output(do, dg,P, Pb, T)
-"""
+OilPhase_Correlations(do=do, dg=dg, API=None, P=P, Pb=Pb, T=T, Units=unidades2).output()
